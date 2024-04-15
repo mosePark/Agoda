f7227482bd5d9f642adc14e12ef3e6ab38eeb181
mosePark <58103517+mosePark@users.noreply.github.com>
2024-04-15 16:58:51 +0900

    Update GPT-Baseline.py

--- GPT-Application/GPT-Baseline.py
+++ GPT-Application/GPT-Baseline.py
@@ -2,7 +2,7 @@ import os
 import openai
 from langchain.chat_models import ChatOpenAI
 
-open_api_key = 'xxx'
+open_api_key = 'your-api-key'
 chat_model = ChatOpenAI(openai_api_key = open_api_key, verbose = False)
 
 content = '이 리뷰를 보고 몇점을 줬을지 예측해줘. 리뷰는 다음과 같아. "Bottom of the 18 meter pool was too scary. Staff were nice though."'
