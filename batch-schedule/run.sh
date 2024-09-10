5c144037c1bd59232cba5f939d3aa9b20ff50439
mosePark <58103517+mosePark@users.noreply.github.com>
2024-09-10 14:12:52 +0900

    Update run.sh

--- batch-schedule/run.sh
+++ batch-schedule/run.sh
@@ -29,3 +29,26 @@ python3 run_seq_cls.py --task question-pair --config_file KcELECTRA-v2022-0404-8
 python3 run_seq_cls.py --task korsts --config_file KcELECTRA-v2022-0404-800k-discriminator.json
 python3 run_ner.py --task naver-ner --config_file KcELECTRA-v2022-0404-800k-discriminator.json
 python3 run_squad.py --task korquad --config_file KcELECTRA-v2022-0404-800k-discriminator.json
+
+
+
+'''
+AL 때 예시
+'''
+
+
+
+#!/bin/bash
+#SBATCH --job-name=AL
+#SBATCH -o SLURM.%N.%j.out
+#SBATCH -e SLURM.%N.%j.err
+#SBATCH --nodes=001
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=gpu3
+
+hostname
+date
+
+module add ANACONDA/2020.11
+
+/home1/mose1103/anaconda3/envs/fine-koactive/bin/python main.py --task active_learning
