0ed1efb0ca31de526be2fdf16af1d01a059ab38f
mosePark <58103517+mosePark@users.noreply.github.com>
2024-10-23 21:15:32 +0900

    Update src.sh

--- Batch-schedule/src.sh
+++ Batch-schedule/src.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=simul0_1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=cpu1
+##
+#SBATCH --job-name=simulation0_1
 #SBATCH -o SLURM.%N.%j.out
 #SBATCH -e SLURM.%N.%j.err
-#SBATCH --nodes=004
-#SBATCH --ntasks-per-node=1
-#SBATCH --partition=gpu1
 
 hostname
 date
 
 
 module add R/4.3.1
+
+export R_LIBS="/home1/mose1103/R/library"
 
 for argv in "$*"
 do
