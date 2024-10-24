d5fcf4a4f48523f9a66ba4a5c0ec89c5822d6037
mosePark <58103517+mosePark@users.noreply.github.com>
2024-10-25 00:15:31 +0900

    Create 24-10-25-data-to-RDS.R

---
+++ preprocessing/24-10-25-data-to-RDS.R
@@ -0,0 +1,9 @@
+save_dir <- "D:/ebd_matrix"
+
+saveRDS(or_emb, file = file.path(save_dir, "or_emb.rds"))
+saveRDS(gen1_emb_0_7, file = file.path(save_dir, "gen1_emb_0_7.rds"))
+saveRDS(gen2_emb_0_7, file = file.path(save_dir, "gen2_emb_0_7.rds"))
+saveRDS(gen1_1_emb_0_7, file = file.path(save_dir, "gen1_1_emb_0_7.rds"))
+saveRDS(gen1_emb_1_5, file = file.path(save_dir, "gen1_emb_1_5.rds"))
+saveRDS(gen2_emb_1_5, file = file.path(save_dir, "gen2_emb_1_5.rds"))
+saveRDS(gen1_1_emb_1_5, file = file.path(save_dir, "gen1_1_emb_1_5.rds"))
