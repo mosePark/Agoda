eef1040a01cf2c126773b37d63a84187347c928c
mosePark <58103517+mosePark@users.noreply.github.com>
2024-12-03 09:11:01 +0900

    Update 24-12-02-hug-data.py

--- embedding/24-12-02-hug-data.py
+++ embedding/24-12-02-hug-data.py
@@ -1,20 +1,59 @@
 #%%
-import pandas as pd
+
 from datasets import load_dataset
+
+import os
+import ast
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from openai import OpenAI
+from dotenv import load_dotenv
 
 
 #%%
+
+'''
+데이터 로드
+'''
+
 dataset = load_dataset("humarin/chatgpt-paraphrases")
 
 df = dataset['train'].to_pandas()
 
+df['source'].value_counts()
+df['category'].value_counts()
 
-source_counts = df['source'].value_counts()
 
 
-# 'source'별 10% 샘플링
-sampled_df = df.groupby('source').apply(lambda x: x.sample(frac=0.1, random_state=42)).reset_index(drop=True)
+# 10% 샘플링
+sdf = (
+    df.groupby(['source', 'category'], group_keys=False)
+      .apply(lambda x: x.sample(frac=0.1, random_state=413))
+)
 
-# 결과 확인
-print("샘플링된 데이터셋 크기:")
-print(sampled_df['source'].value_counts())
+idx = sdf.index
+
+sdf['source'].value_counts()
+sdf['category'].value_counts()
+
+sdf["paraphrases"] = sdf["paraphrases"].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
+print(sdf["paraphrases"].head())
+
+sdf["gen1"] = sdf["paraphrases"].apply(lambda x: x[0] if len(x) > 0 else None)
+sdf["gen2"] = sdf["paraphrases"].apply(lambda x: x[1] if len(x) > 1 else None)
+
+sdf.columns
+
+sdf.info()
+sdf.head()
+
+sdf.iloc[1, :]['paraphrases']
+sdf.iloc[1, :]['gen1']
+sdf.iloc[1, :]['gen2']
+
+
+# sdf_with_idx = sdf.reset_index().rename(columns={'index': 'idx'})
+# sdf_with_idx.to_csv('hug.csv', encoding='utf-8-sig', index=False)
+# sdf_with_idx.to_excel('hug.xlsx', index=False)
