62bb893864b075c464fbee1375bc6cc25b9bc32b
mosePark <58103517+mosePark@users.noreply.github.com>
2024-12-17 17:16:51 +0900

    Update comparison_tests.R

--- revision/comparison_tests.R
+++ revision/comparison_tests.R
@@ -169,46 +169,6 @@ for (i in 1:kfds) {
 
 # Final
 
-perform_folds_tests <- function(source = 'quora', X, Y, kfds = 5, method = c("eqdist", "ball", "kernel")) {
-  set.seed(123)
-  idx <- which(df$source == source)
-  X <- X[idx, , drop = FALSE]
-  Y <- Y[idx, , drop = FALSE]
-  
-  n <- nrow(X)
-  folds <- split(sample(1:n), rep(1:kfds, length.out = n))
-  
-  res <- list()
-  
-  for (i in 1:kfds) {
-    current_idx <- folds[[i]]
-    X_part <- X[current_idx, , drop = FALSE]
-    Y_part <- Y[current_idx, , drop = FALSE]
-    
-    if (method == "eqdist") {
-      combined_data <- rbind(X_part, Y_part)
-      test_result <- energy::eqdist.etest(dist(combined_data), sizes = c(nrow(X_part), nrow(Y_part)), R = 99)
-      res[[paste0("Fold_", i)]] <- test_result
-      cat("\n### Fold", i, "(eqdist) ###\n")
-      cat("p-value:", test_result$p.value, "\n")
-      
-    } else if (method == "ball") {
-      test_result <- Ball::bd.test(X_part, Y_part)
-      res[[paste0("Fold_", i)]] <- test_result
-      cat("\n### Fold", i, "(ball) ###\n")
-      cat("p-value:", test_result$p.value, "\n")
-      
-    } else if (method == "kernel") {
-      sig <- med_sigma(X_part, Y_part)
-      test_result <- kerTests::kertests(X_part, Y_part, sigma = sig)
-      res[[paste0("Fold_", i)]] <- test_result
-      cat("\n### Fold", i, "(kernel) ###\n")
-      cat("p-value:", test_result$p.value, "\n")
-    }
-  }
-  return(res)
-}
-
 comparison_tests <- function(source = 'quora', X, Y, kfds = 5, method = c("eqdist", "ball", "kernel"), seed = 123) {
   set.seed(seed)
   idx <- which(df$source == source)
